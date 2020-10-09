e4ae06f7fd334029502b96eb39108eb23b70708c
shellcya <shellcya_32@hotmail.com>
2020-10-09 17:40:19 -0300

    modificacoes

--- mysite/src/components/Portifólio/Portifólio.js
+++ mysite/src/components/Portifólio/Portifólio.js
@@ -3,13 +3,26 @@ import "./Portifólio.css"
 
 import counter from "../../assets/img/counter.png"
 import jokenpo from "../../assets/img/jokenpo.jpg"
-
+import huntweb from "../../assets/img/huntweb.jpg"
 const Portifólio = () => {
   return (
     <div >
       <h3 className="title3">Projetos Front-end</h3>
 
+
+
+
       <div className="project">
+
+
+        <a className="hb"
+          href="https://github.com/Shellcya/Huntweb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={huntweb} alt="imagem do huntweb" />Huntweb
+      </a>
+
         <a
           href="https://github.com/Shellcya/Contador"
           target="_blank"
@@ -26,6 +39,8 @@ const Portifólio = () => {
         >
           <img src={jokenpo} alt="imagem do jokenpo" />PedraPapelTesoura
       </a>
+
+
 
       </div>
 
